dcf8c5a7513e031a7955d5341f9598b3a63d7f2a
Luizaescarban <luizaescarbandasil@gmail.com>
2025-04-30 14:49:13 -0300

    Feat: Termino do projeto

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -2,3 +2,4 @@ from .user import UserSerializer
 from .acessorio import AcessorioSerizalizer
 from .cor import CorSerializer
 from .modelo import ModeloSerializer
+from .veiculo import VeiculoSerializer
